ad745590980972ebfe1f7fe6df500067ce08dc4d
Frederic Migeon <frederic.migeon@irit.fr>
2022-10-02 11:10:14 +0200

    Created domain creation scenario

---
+++ src/scenarios/CreatingAndAccessingExemplaries.java
@@ -0,0 +1,25 @@
+package scenarios;
+
+import domaine.Bibliotheque;
+import domaine.Exemplaire;
+import domaine.Ouvrage;
+
+public class CreatingAndAccessingExemplaries {
+
+	public static void main(String[] args) {
+
+		Bibliotheque maBibliotheque = new Bibliotheque(50);
+
+		Ouvrage bse = new Ouvrage("Beginning Software Engineering", "Rod Stephens", "wrox", 2015, "978-1-1118-969114-454500");
+		
+		Exemplaire bseAConsulter = new Exemplaire(getNewCote());
+		
+	}
+
+// Les cotes des exemplaires seront implantés par un compteur tout simple qui augmente à chaque appel
+	private static String getNewCote() {
+		return ++coteCount+"";
+	}
+	private static int coteCount = 0;
+
+}
